8941f259d82d4fd8f8cd2b68fdb3cf9421d320c0
skymeson <skymeson@gmail.com>
2015-07-07 10:39:04 -0400

    First commit for data insight challenge

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,1 +1,24 @@
-# example of program that calculates the median number of unique words per tweet.
+#!/usr/bin/python
+
+def median(mylist):
+    sorts = sorted(mylist)
+    length = len(sorts)
+    if not length % 2:
+        return (sorts[length / 2] + sorts[length / 2 - 1]) / 2.0
+    return sorts[length / 2]
+
+def read_words(words_file):
+        return [word for line in open(words_file, 'r') for word in line.split()]
+
+
+file_input=open("tweet_input/tweets.txt","r")
+file_output=open("tweet_output/ft2.txt","w")
+
+
+unique_word_count = []
+
+for tweet in file_input:
+    unique_word_count.append(len(set(w.lower() for w in tweet.split())))
+    print median(unique_word_count)
+    file_output.write( str(median(unique_word_count)) + "\n")
+
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python
+
+
+from collections import defaultdict
+
+
 # example of program that calculates the total number of times each word has been tweeted.
+
+#file=open("../tweet_input/tweets.txt","r")
+file_input=open("tweet_input/tweets.txt","r")
+file_output=open("tweet_output/ft1.txt","w")
+
+wordcount={}
+for word in file_input.read().split():
+        if word not in wordcount:
+               wordcount[word] = 1
+        else:
+                wordcount[word] += 1
+
+
+for key in sorted(wordcount):
+    print "%s \t\t %s" % (key.ljust(20), wordcount[key])
+    file_output.write("%s \t\t %s \n" % (key.ljust(20), wordcount[key]))
+
+
+file_input.close()
+file_output.close()
