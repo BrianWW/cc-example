3dd73fe1d18ca99e044c380d1871be704c18b871
skymeson <skymeson@gmail.com>
2015-07-11 00:08:26 -0400

    Updated file IO to include command line option.

--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python
 
-
+import sys
 from collections import defaultdict
 
 
 # example of program that calculates the total number of times each word has been tweeted.
 
-#file=open("../tweet_input/tweets.txt","r")
-file_input=open("tweet_input/tweets.txt","r")
-file_output=open("tweet_output/ft1.txt","w")
+#file_input=open("tweet_input/tweets.txt","r")
+#file_output=open("tweet_output/ft1.txt","w")
 
-wordcount={}
-for word in file_input.read().split():
+
+def main():
+    if len(sys.argv) == 3:
+        file_input = open(sys.argv[1],"r")
+        file_output = open(sys.argv[2],"w")
+    elif len(sys.argv) == 1:
+        file_input=open("tweet_input/tweets.txt","r")
+        file_output=open("tweet_output/ft1.txt","w")
+    else:
+        print 'usage: ./words_tweeted.py fileinput fileoutput'
+        sys.exit(1)
+
+
+    wordcount={}
+    for word in file_input.read().split():
         if word not in wordcount:
-               wordcount[word] = 1
+            wordcount[word] = 1
         else:
-                wordcount[word] += 1
+            wordcount[word] += 1
 
 
-for key in sorted(wordcount):
-    print "%s \t\t %s" % (key.ljust(20), wordcount[key])
-    file_output.write("%s \t\t %s \n" % (key.ljust(20), wordcount[key]))
+    for key in sorted(wordcount):
+        print "%s \t\t %s" % (key.ljust(20), wordcount[key])
+        file_output.write("%s \t\t %s \n" % (key.ljust(20), wordcount[key]))
 
 
-file_input.close()
-file_output.close()
+
+    file_input.close()
+    file_output.close()
+
+if __name__ == '__main__':
+    main()
